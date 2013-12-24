6e11322f763e492d08cda79c414c83b05509accb
Ronak Desai <rdesai@klick.com>
2013-12-24 07:46:38 -0500

    dsi.js modular common tasks

---
+++ js/dsi.js
@@ -0,0 +1,21 @@
+var util = require('util'),
+	colors = require('colors'),
+	config = require('../config.json');
+
+var dsi = module.exports = function() {
+	var self = this;
+}
+
+dsi.prototype.log = function(cmd, colorize) {
+	/*-- if object color it --*
+	if(typeof cmd === "object") util.inspect(cmd, true, null, true);
+	/*-- --*/
+	console.log(cmd);
+};
+
+dsi.prototype.logDash = function(dashLen) {
+	dashLen = dashLen || config.dashLen;
+	console.log(Array(dashLen).join('-'));
+};
+
+dsi.prototype.dir = function(cmd) { console.dir(cmd); };
